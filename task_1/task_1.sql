25cb43a2cfea130a5aa4f9108e479d619e08e35c
Viktoria Orekhova <vika.savina.2000@inbox.ru>
2025-03-20 21:52:13 +0300

    Update task_1.sql

--- task_1/task_1.sql
+++ task_1/task_1.sql
@@ -1,3 +1,60 @@
+-- Создание таблицы Violation_a
+CREATE TABLE Violation_a (
+    dt TIMESTAMP,          
+    employer BIGINT,       
+    users BIGINT,           
+    attempts_count INT,    
+    PRIMARY KEY (dt, employer, users) 
+);
+
+-- Создание таблицы Violation_b
+CREATE TABLE Violation_b (    
+    date VARCHAR(10),           
+    employer BIGINT,       
+    users BIGINT,           
+    views_count INT,       
+    PRIMARY KEY (date, employer, users) 
+);
+
+-- Создание таблицы Violation_с
+CREATE TABLE Violation_c (
+    dt TIMESTAMP,         
+    employer VARCHAR(50),       
+    users BIGINT,           
+    has_access BOOLEAN,  
+    PRIMARY KEY (dt, employer, users) 
+);
+
+-- Шаг 1
+SELECT 
+    DATE(dt) AS day, 
+    employer, 
+    COUNT(DISTINCT users) AS violators_a, 
+    SUM(attempts_count) AS attempts_count
+FROM Violation_a
+GROUP BY DATE(dt), employer;
+
+-- Шаг 2
+SELECT 
+    date AS day, 
+    employer, 
+    COUNT(DISTINCT users) AS violators_b, 
+    SUM(views_count) AS views_count
+FROM Violation_b
+GROUP BY date, employer
+HAVING SUM(views_count) > 500;
+
+-- Шаг 3
+SELECT 
+	DATE(dt) AS day, 
+	employer::bigint AS employer, 
+	COUNT(DISTINCT CASE WHEN has_access = TRUE THEN users END) AS violators_with_access, 
+	COUNT(DISTINCT CASE WHEN has_access = FALSE THEN users END) AS violators_without_access
+FROM Violation_c
+GROUP BY DATE(dt), employer::bigint
+HAVING COUNT(DISTINCT CASE WHEN has_access = TRUE THEN users END) >= 2
+
+-- Шаг 4
 WITH violations_a AS (
     SELECT 
         DATE(dt) AS day, 
@@ -19,10 +76,10 @@ violations_b AS (
 ),
 violations_c AS (
     SELECT 
-        DATE(dt) AS day, 
-        employer::bigint AS employer, 
-        COUNT(DISTINCT CASE WHEN has_access = TRUE THEN users END) AS violators_with_access, 
-        COUNT(DISTINCT CASE WHEN has_access = FALSE THEN users END) AS violators_without_access
+    	DATE(dt) AS day, 
+    	employer::bigint AS employer, 
+    	COUNT(DISTINCT CASE WHEN has_access = TRUE THEN users END) AS violators_with_access, 
+    	COUNT(DISTINCT CASE WHEN has_access = FALSE THEN users END) AS violators_without_access
     FROM Violation_c
     GROUP BY DATE(dt), employer::bigint
     HAVING COUNT(DISTINCT CASE WHEN has_access = TRUE THEN users END) >= 2
